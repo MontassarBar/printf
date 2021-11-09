4115f400eecc36f61c027d17e887c3e4c64e7ce2
MounaBenAli <benalimouna1986@gmail.com>
2021-11-09 09:06:36 -0800

    functions

--- functions.c
+++ functions.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+
 /**
  * count_d10 - count number of digits
  *
@@ -23,6 +24,7 @@ int count_d10(int n)
 	}
 	return (x);
 }
+
 /**
 * _putchar - writes the character c to stdout
 * @c: The character to print
@@ -35,7 +37,6 @@ int _putchar(char c)
 return (write(1, &c, 1));
 }
 
-
 /**
 *_abs - abs
 *@n: var
@@ -46,24 +47,8 @@ int _abs(int n)
 		return (-n);
 	return (n);
 }
-/**
-**_strchr - locates a character in a string
-*loop
-*@s: pointer
-*@c: var
-*Return: s
-**/
-char *_strchr(char *s, char c)
-{
-while ((*s != c) && (*s != '\0'))
-{
-s++;
-}
-if (*s == c)
-return (s);
-else
-return (NULL);
-}
+
+
 /**
  * _strlen - get length of string
  *
@@ -81,161 +66,7 @@ i++;
 }
 return (i);
 }
-/**
-*_strcmp - compares two strings
-*loop
-*@s1: pointer
-*@s2: pointer
-*Return: a
-**/
-int _strcmp(char *s1, char *s2)
-{
-int x, y, z, a;
-y = _strlen(s1);
-z = 0;
-a = 0;
-for (x = 0; ((x < y)&&(z == 0)); x++)
-{
-if (s1[x] != s2[x])
-z = 1;
-if (z == 1)
-a = s1[x] - s2[x];
-}
-return (a);
-}
-/**
-*_strspn - gets the length of a prefix substring
-*loops
-*@s: pointer
-*@accept: pointer
-*Return: r
-**/
-unsigned int _strspn(char *s, char *accept)
-{
-int x, y, z, a, r;
-y = _strlen(s);
-a = _strlen(accept);
-r = 0;
-for (x = 0; x < y; x++)
-{
-for (z = 0; z < a; z++)
-{
-if (s[x] == accept[z])
-break;
-}
-if (s[x] == accept[z])
-r++;
-else if (s[x] != accept[z])
-break;
-}
-return (r);
-}
-/**
-**_strpbrk - searches a string for any of a set of bytes
-*loops
-*@s: pointer
-*@accept: pointer
-*Return: s+x or NULL
-**/
-char *_strpbrk(char *s, char *accept)
-{
-int x, i, y, z;
-y = _strlen(s);
-z = _strlen(accept);
-for (x = 0; x < y; x++)
-{
-for (i = 0; i < z; i++)
-{
-if (s[x] == accept[i])
-return (s + x);
-}
-}
-return (NULL);
-}
-/**
-**_strstr - locates a substring
-*loop
-*@haystack: pointer;
-*@needle: pointer
-*Return: h or Null
-**/
-char *_strstr(char *haystack, char *needle)
-{
-int y, z;
-z = _strlen(haystack);
-for (y = 0; y < z; y++)
-{
-if (*needle == haystack[y])
-return (haystack + y);
-}
-return (NULL);
-}
-/**
-**_strcpy - copies the string
-*pointerrrrs
-*@dest: pointers
-*@src: pointers
-*Return: dest
-**/
-char *_strcpy(char *dest, char *src)
-{
-int x, y, z;
-y = _strlen(src);
-z = 0;
-for (x = 0; x <= y; x++)
-{
-dest[z] = src[x];
-z++;
-}
-return (dest);
-}
-#include <stdio.h>
-/**
- * print_number - hi
- * @n: hi
- */
-void print_number(int n)
-{
-int x, n1, d, r;
-n1 = n;
-if (n == 0)
-_putchar(n + 48);
-else if ((n > 0) && (n < 10))
-{
-x = n1 % 10;
-_putchar(x + 48);
-}
-else if ((n < 0) && (n > -10))
-{
-n1 = -n1;
-x = n1 % 10;
-_putchar('-');
-_putchar(x + 48);
-}
-else
-{
-if (n < 0)
-{
-_putchar('-');
-d = -1;
-}
-else
-d = 1;
-r = 11;
-while (r >= 10)
-{
-d = d * 10;
-r = n1 / d;
-}
-while ((d > 0) || (d < 0))
-{
-x = n1 / d;
-n1 = n1 % d;
-_putchar(x + 48);
-d = d / 10;
-}
-}
-}
+
 /**
  * _itoa - int to str
  *
@@ -245,12 +76,7 @@ char *_itoa(int n)
 {
 	char *s;
 	int x = 0,
-	y = 0,
-	c = 0,
-	i = 0,
-	b = _abs(n),
-	l = 0,
-	e = 1;
+	y = 0,c = 0,i = 0,b = _abs(n),l = 0,e = 1;
 
 	while (b != 0)
 	{
@@ -279,7 +105,6 @@ char *_itoa(int n)
 		s[i] = c;
 		i++;
 	}
-
 	s[i] = '\0';
 
 	return (s);
