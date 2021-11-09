02d6eaec7701c39f7a7e55105376687a86ead03f
MounaBenAli <benalimouna1986@gmail.com>
2021-11-09 08:44:04 -0800

    functions

--- functions.c
+++ functions.c
@@ -45,11 +45,7 @@ return (write(1, &c, 1));
 char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int x;
-for (x = 0; x < n; x++)
-s[x] = b;
 
-return (s);
-}
 /**
 * *_strcat -  concatenates two strings
 *loop
